ade67d5c075f19ec9b14eea12b0025b9ba781c66
ianhnizdo <ianhnizdo@gmail.com>
2023-02-02 17:59:31 -0500

    Minimum Substring

--- Minimum-Substring.py
+++ Minimum-Substring.py
@@ -8,6 +8,7 @@ class Solution:
             sobj[t[i]]=0
             tobj[t[i]]=1+tobj.get(t[i],0)
         
+
         res, resLen= [-1,-1], float("infinity")
         
         matches, total = 0, len(t)
@@ -24,32 +25,62 @@ class Solution:
             
             if cur in tobj and sobj[cur]==tobj[cur]:
                     matches+=1
-            while(have==need):
-
+            
+            if matches == total:
+                print(l,r, s[l:r+1])
+                if r-l+1<resLen:
+                    resLen=r-l+1
+                    res = [l,r]
                 
-                if matches==total:
-                    print('hit')
-                    arr.append([l,r])
-                    
-
-                    # Now we need to move the left pointer up
-                    sobj[s[l]]=sobj.get(s[l])-1
-                    l+=1
-                    
-                    while(l<r):
-                        if s[l] in tobj: break
-                        sobj[s[l]]=sobj.get(s[l])-1
-                        l+=1
-                smallest = min(len(smallest), len(modern))
-                # Now we need to move the left pointer up
                 sobj[s[l]]=sobj.get(s[l])-1
+                if tobj.get(s[l],0)>0 and sobj[s[l]]<tobj[s[l]]: matches-=1
                 l+=1
                 while(l<r):
-                    if s[l] in sobj: break
+                    print(l,r)
+                    # Match
+                    if(tobj.get(s[l],0)>0): break
+                    sobj[s[l]]=sobj.get(s[l])-1
                     l+=1
-                # Some logic to make smallest equal to the string of l to r
-                modern=s[l:r]
-        return
+                if matches == total and r-l+1<resLen:
+                    resLen=r-l+1
+                    res = [l,r]
+            # print(sobj, matches, resLen, res)
+        print(res, resLen, s[9:12])
+        if resLen==float('infinity'):
+            return ""
+        return s[l-1:r+1]
+
+class Solution:
+    def minwindow(self, s : str, t: str)-> str:
+        if t=="": return ""
+        
+        countT, countS = {}, {}
+
+        for c in t:
+            countT=1+countT.get(c,0)
+        
+        matches, total = 0, len(countT)
+
+        res, resLen = [-1,-1], float("infinity")
+        l=0
+        for r in range(len(s)):
+            c=s[r]
+            countS=1+countS.get(c,0)
+
+            if c in countT and countS[c]==countT[c]:
+                matches+=1
+            
+            while matches==total:
+                # update
+                if(r-l+1)<resLen:
+                    res=[l,r]
+                    resLen=(r-l+1)
+                countS[s[l]]=countS.get(s[l])-1
+                if s[l] in countT and countS[s[l]]<countT[s[l]]:
+                    matches-=1
+                l+=1
+        l,r=res
+        return s[l:r+1] if resLen != float("infinity") else ""
 
 # Step 1. Understand the problem
 # 
