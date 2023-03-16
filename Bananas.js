7cdb90cc46d1b27faa2584544dc2b4a8f32ba37b
ian <ianr.hnizdo@gmail.com>
2023-03-15 20:13:33 -0400

    Fixed Bananas

--- Bananas.js
+++ Bananas.js
@@ -4,58 +4,6 @@
  * @return {number}
  */
 
-//This doesn't work in all test cases
-/*
-var minEatingSpeed = function(piles, h) {
-
-    if(h === piles.length) return Math.max(...piles);
-    
-    let start = 1;
-    let end = Math.max(...piles);
-    let mid = Math.floor((start+end)/2);
-    // console.log(mid);
-    
-    while(start<end){
-    
-    //keep track of bananna pile
-    let sum=0;
-    
-    //i is our index
-    let i =0;
-    
-    //j is our h
-    let j=0;
-    
-    while(j<h && i<piles.length){
-        sum+=mid;
-        // console.log(piles[i], sum);
-    
-        if(sum>=piles[i]) {
-            i++;
-            sum=0
-        }
-        j++;
-    }
-    
-    // console.log(i);
-    if(i<piles.length){
-        start+=1;
-        mid=Math.floor((start+end)/2);
-    }else if(j>piles.length){
-        end-=1;
-        mid=Math.floor((start+end)/2);
-    }
-    //Winning formula
-    else break;
-    
-    }
-    
-    return mid;
-    
-    };
-    
-*/
-
 
 var minEatingSpeed = function(piles, h) {
 
@@ -67,12 +15,19 @@ var minEatingSpeed = function(piles, h) {
     let end = max;
     
     let r = end;
-    // console.log(mid);
     
-    while(start<end){
+    while(start<=end){
     
         const mid = Math.floor((start+end)/2);
-        const getHour = hourSpend(mid, piles)
+        const getHour = hourSpent(mid, piles);
+    
+    
+        if(h<getHour){
+            start=mid+1
+        }else if(getHour<=h){
+            end=mid-1
+            r=mid
+        }
     
     }
     
@@ -87,3 +42,5 @@ var minEatingSpeed = function(piles, h) {
         }
         return hours;
     }
+
+    console.log(minEatingSpeed([3,6,7,11],8)) //should be 4
